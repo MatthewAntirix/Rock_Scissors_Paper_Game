90d019fa2c538485476a5e251d3417f3e886cce6
Matthew Antirix <matthew.antirix@gmail.com>
2023-02-11 21:35:08 +0100

    in progress

--- src/App.js
+++ src/App.js
@@ -14,21 +14,60 @@ let languageSwitch = "en"
 
 export const StoneScissorsPaperGame = () => {
   const stance = [language.stance.aggressive, language.stance.sneaky, language.stance.defensive]
-  const [playerChoose, setPlayerChoose] = useState('')
+  const [playerChoose, setPlayerChoose] = useState(-1)
   const [computerChoose, setComputerChoose] = useState(Math.floor(Math.random() * stance.length))
-    let winner
+  const [computerStep, setComputerStep] = useState(0)
+  const [computerHealth, setComputerHealth] = useState(2)
+  const [winnerStatus, setWinnerStatus] = useState('')
+  const [winner, setWinner] = useState('')
+  const [endGame, setEndGame] = useState(false)
+  const [battleField] = useState(["C", "", "", "P"])
     let playerStance = (stance[playerChoose])
     let computerStance = (stance[computerChoose])
 
-  if (stance.indexOf(playerStance) === stance.indexOf(computerStance)) {
-    winner = "draw"
-  } else if (stance.indexOf(playerStance) === stance.indexOf(computerStance)-1 || 
-            (stance.indexOf(playerStance) === stance.indexOf(computerStance)+2)) {
-    winner = "player"
-  } else if (stance.indexOf(playerStance) === stance.indexOf(computerStance)+1 || 
-            (stance.indexOf(playerStance) === stance.indexOf(computerStance)-2)) {
-    winner = "computer"
+
+  if (endGame === false) {
+    if (playerChoose !== -1) {
+      if (stance.indexOf(playerStance) === stance.indexOf(computerStance)) {
+        setWinnerStatus("draw")
+        setComputerHealth(computerHealth + 1)
+        setPlayerChoose(-1)
+      } 
+      else if (stance.indexOf(playerStance) === stance.indexOf(computerStance)-1 || 
+                (stance.indexOf(playerStance) === stance.indexOf(computerStance)+2)) {
+        setWinnerStatus("player")
+        setComputerHealth(computerHealth - 1)
+        setPlayerChoose(-1)
+      } else if (stance.indexOf(playerStance) === stance.indexOf(computerStance)+1 || 
+                (stance.indexOf(playerStance) === stance.indexOf(computerStance)-2)) {
+        setWinnerStatus("computer")
+        setPlayerChoose(-1)
+      }
+    }
+
+    if (computerHealth === 0 ) {
+      setEndGame(true)
+      setWinner("Player")
+    }
   }
+
+  useEffect(()=>{
+    if (winnerStatus === "computer") {
+      battleField[computerStep] = ""
+      battleField[computerStep +1] = "C"
+      setComputerStep(computerStep +1)
+      setComputerChoose(Math.floor(Math.random() * stance.length))
+      setWinnerStatus('')
+
+      if (!battleField.includes('P')) {
+        setEndGame(true)
+        setWinner("Computer")
+      }
+    }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [winnerStatus]);
+ 
+
 
   return (
     <>
@@ -38,6 +77,16 @@ export const StoneScissorsPaperGame = () => {
       {stance.map((item, index) =>
         <button key={index} value={index} onClick={e => setPlayerChoose(e.target.value)}>{item}</button>
       )}
+health{computerHealth}
+      <table className='test'>
+        <tbody>
+          <tr>
+          {battleField.map((item, index) =>
+            <td key={index}>{item}</td>
+          )}
+          </tr>
+        </tbody>
+      </table>
 
       <table>
         <thead>
@@ -62,7 +111,7 @@ export const StoneScissorsPaperGame = () => {
         </tbody>
       </table> 
     
-    result {winner}
+    result {winnerStatus} //// {computerStance}
     </>
   )
 }
