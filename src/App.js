f44e4dd347f77f109e7a69aa6fd10669d3178b57
Matthew Antirix <matthew.antirix@gmail.com>
2023-02-11 14:27:21 +0100

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react'
+import {cz} from "./language_cz"
+import {en} from "./language_en"
+import "./basic.css"
 
-function App() {
+export const StoneScissorsPaperGame = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    `xx`
+  )
 }
-
-export default App;
